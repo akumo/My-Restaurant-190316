de5a8f31822186034678504e6a45875b2422a691
akumo <akumo@yahoo.com>
2016-03-19 11:55:26 +0700

    Delete all sqlite

--- app/src/main/java/com/akumoshop/akumo/myrestaurant/MainActivity.java
+++ app/src/main/java/com/akumoshop/akumo/myrestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.akumoshop.akumo.myrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,9 +18,18 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(this);
 
         //Test Add Value
-        testAddValue();
+        //testAddValue();
 
+        //Delete All SQLite
+        deleteAllSQLite();
     }//Main Method
+
+    private void deleteAllSQLite() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+        sqLiteDatabase.delete(MyManage.food_table, null, null);
+    }
 
     private void testAddValue() {
         myManage.addValue(1, "user", "pass", "name");
