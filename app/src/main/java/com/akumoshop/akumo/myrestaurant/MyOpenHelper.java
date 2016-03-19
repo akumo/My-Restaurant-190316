a31a1b8a4cfe5c9f7f7faaec48a0300097d6106f
akumo <akumo@yahoo.com>
2016-03-19 10:32:46 +0700

    Create OpenHelper

---
+++ app/src/main/java/com/akumoshop/akumo/myrestaurant/MyOpenHelper.java
@@ -0,0 +1,41 @@
+package com.akumoshop.akumo.myrestaurant;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by akumo on 19-Mar-16.
+ */
+public class MyOpenHelper extends SQLiteOpenHelper{
+
+    //Explicit
+    public static final String database_name = "Restaurant.db";
+    private static final int database_version = 1;
+    private static final String create_user_table = "create table userTABLE (" +
+            "_id integer primary key, " +
+            "User text, " +
+            "Password text, " +
+            "Name text);";
+
+    private static final String create_food_table = "create table foodTABLE (" +
+            "_id integer primary key, " +
+            "Food text, " +
+            "Price text);";
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null, database_version);
+    }//Constructor
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_user_table);
+        sqLiteDatabase.execSQL(create_food_table);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}//Main Class
